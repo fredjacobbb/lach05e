bba72458eae0eb1d940039876253eb10edfd4994
fredjacobbb <frdjacobbb@gmail.com>
2025-08-03 18:34:11 +0200

    Day 3

--- pkg/utils/RequestAssault.go
+++ pkg/utils/RequestAssault.go
@@ -2,10 +2,13 @@ package utils
 
 import (
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/pterm/pterm"
 )
+
+var errorStyle = pterm.NewStyle(pterm.FgWhite, pterm.BgRed)
 
 type Request struct {
 	Url          string
@@ -35,10 +38,10 @@ func RequestAssault(r Request) {
 		return
 	}
 	if resp.StatusCode != 200 {
-		pterm.FgRed.Printf("%s %s\n", resp.Status, r.Url+pterm.White(r.Payload))
+		pterm.Printf("%s %s\n", pterm.NewStyle(pterm.BgRed, pterm.FgWhite).Sprint(resp.Status), pterm.NewStyle(pterm.FgWhite).Sprint("https://"+strings.TrimSpace(r.Url)+strings.TrimSpace(r.Payload)))
 		return
 	}
 
-	pterm.FgGreen.Printf("%s %s\n", resp.Status, r.Url+pterm.White(r.Payload))
+	pterm.FgGreen.Printf("%s %s\n", pterm.NewStyle(pterm.BgGreen, pterm.FgWhite).Sprint(resp.Status), pterm.NewStyle(pterm.FgWhite).Sprint("https://"+strings.TrimSpace(r.Url)+strings.TrimSpace(r.Payload)))
 
 }
